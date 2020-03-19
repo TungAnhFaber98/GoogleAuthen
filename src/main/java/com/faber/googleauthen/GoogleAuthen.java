6ce4fc07328da3120cb7ae0d84b3c9233b25c1d3
TungAnhFaber98 <phamtunganh@fabercompany.co.jp>
2020-03-19 15:05:35 +0700

    first commit

---
+++ src/main/java/com/faber/googleauthen/GoogleAuthen.java
@@ -0,0 +1,16 @@
+package com.faber.googleauthen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author TungAnhFaber
+ */
+
+@SpringBootApplication
+public class GoogleAuthen {
+    public static void main(String[] args){
+        SpringApplication.run(GoogleAuthen.class, args);
+    }
+}
